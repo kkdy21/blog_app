35e35a41454da3e54f835832f6ecebfa876191e7
daeyeon ko <rowan@megazone.com>
2025-08-04 23:54:01 +0900

    feat: 기본 model 정의

---
+++ src/model/blog/database.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class BlogData:
+    id: int
+    title: str
+    author: str
+    content: str
+    modified_dt: datetime
+    image_loc: str | None = None
