cede908b15a80a9d5c3e0989c1d7b6fd667aef02
daeyeon ko <rowan@megazone.com>
2025-08-14 18:51:04 +0900

    feat: 라우팅 에러 핸들러 추가

--- src/utils/error_handler.py
+++ src/utils/error_handler.py
@@ -2,6 +2,7 @@ from fastapi import HTTPException, status
 from fastapi.exceptions import RequestValidationError
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from src.utils.jinja_template import jinja_manager
 
@@ -41,4 +42,30 @@ async def custom_validation_exception_handler(
             "title_message": "입력 값에 문제가 있습니다.",
         },
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+    )
+
+
+async def custom_starlette_http_exception_handler(
+    request: Request, exc: StarletteHTTPException
+) -> HTMLResponse:
+    status_code = exc.status_code
+    title = "에러 발생"
+    detail: str | list[str] = (
+        exc.detail if exc.detail else "요청 처리 중 오류가 발생했습니다."
+    )
+
+    if status_code == status.HTTP_404_NOT_FOUND:
+        title = "페이지를 찾을 수 없습니다."
+        if not exc.detail:
+            detail = "요청하신 주소가 올바르지 않습니다."
+
+    return jinja_manager.templates.TemplateResponse(
+        request=request,
+        name="http_error.html",
+        context={
+            "status_code": status_code,
+            "detail": detail,
+            "title_message": title,
+        },
+        status_code=status_code,
     )
