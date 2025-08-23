8bd326f527a9e4a91dcadab3a58136a5f1427805
kkdy21 <kkdy21@naver.com>
2025-08-23 18:23:16 +0900

    feat: session 방식 비즈니스로직 적용 및 api별 authorization 적용

--- src/router/blog.py
+++ src/router/blog.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter, Depends, File, Form, Request, UploadFile, status
 from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from sqlalchemy.ext.asyncio import AsyncConnection
 
-from src.manager.session_manager import SessionManager
+from src.dependencies.auth import get_current_user, get_current_user_or_none
 from src.model.blog.database import BlogData
+from src.model.user.database import UserData
 from src.service.blog_svc import BlogService
 from src.utils.db.db import get_connection_db
 from src.utils.jinja_template import jinja_manager
@@ -17,8 +18,8 @@ template = jinja_manager.templates
 @router.get("/")
 async def get_all_blogs(
     request: Request,
+    current_user: UserData | None = Depends(get_current_user_or_none),
     conn: AsyncConnection = Depends(get_connection_db),
-    session_manager: SessionManager = Depends(SessionManager),
 ) -> HTMLResponse:
     all_blogs_dto = await BlogService().get_all_blogs(conn)
     return template.TemplateResponse(
@@ -27,7 +28,7 @@ async def get_all_blogs(
         context={
             "request": request,
             "all_blogs": all_blogs_dto,
-            "session_user": session_manager.get_session_user(),
+            "session_user": current_user,
         },
         status_code=200,
     )
@@ -38,8 +39,8 @@ async def get_all_blogs(
 async def get_blog_by_id(
     request: Request,
     blog_id: int,
+    current_user: UserData | None = Depends(get_current_user_or_none),
     conn: AsyncConnection = Depends(get_connection_db),
-    session_manager: SessionManager = Depends(SessionManager),
 ) -> HTMLResponse:
     blog_dto = await BlogService().get_blog_by_id(blog_id, conn)
 
@@ -49,7 +50,7 @@ async def get_blog_by_id(
         context={
             "request": request,
             "blog": blog_dto,
-            "session_user": session_manager.get_session_user(),
+            "session_user": current_user,
         },
         status_code=200,
     )
@@ -58,25 +59,25 @@ async def get_blog_by_id(
 # 블로그 생성
 @router.get("/new")
 def get_create_blog_ui(
-    request: Request, session_manager: SessionManager = Depends(SessionManager)
+    request: Request,
+    current_user: UserData = Depends(get_current_user),
 ) -> HTMLResponse:
     return template.TemplateResponse(
         request=request,
         name="new_blog.html",
-        context={"session_user": session_manager.get_session_user()},
+        context={"session_user": current_user},
     )
 
 
 @router.post("/new")
 async def create_blog(
-    request: Request,
+    current_user: UserData = Depends(get_current_user),
     title: str = Form(min_length=2, max_length=200),
     content: str = Form(min_length=2, max_length=4000),
     image_file: UploadFile = File(None),
     conn: AsyncConnection = Depends(get_connection_db),
-    session_manager: SessionManager = Depends(SessionManager),
 ) -> RedirectResponse:
-    await BlogService().create_blog(title, content, image_file, conn, session_manager)
+    await BlogService().create_blog(title, content, image_file, conn, current_user)
     return RedirectResponse(url="/blogs", status_code=status.HTTP_303_SEE_OTHER)
 
 
@@ -86,33 +87,29 @@ async def get_update_blog_ui(
     request: Request,
     blog_id: int,
     conn: AsyncConnection = Depends(get_connection_db),
-    session_manager: SessionManager = Depends(SessionManager),
+    current_user: UserData = Depends(get_current_user),
 ) -> HTMLResponse:
     blog_dto: BlogData = await BlogService().get_blog_by_id(blog_id, conn)
 
     return template.TemplateResponse(
         request=request,
         name="modify_blog.html",
-        context={
-            "blog": blog_dto,
-            "session_user": session_manager.get_session_user(),
-        },
+        context={"blog": blog_dto, "session_user": current_user},
     )
 
 
 @router.post("/modify/{blog_id}")
 async def update_blog(
-    request: Request,
     blog_id: int,
     title: str = Form(min_length=2, max_length=200),
-    author: str = Form(max_length=100),
+    author_id: int = Form(max_length=100),
     content: str = Form(min_length=2, max_length=4000),
     image_file: UploadFile = File(None),
     conn: AsyncConnection = Depends(get_connection_db),
-    session_manager: SessionManager = Depends(SessionManager),
+    current_user: UserData = Depends(get_current_user),
 ) -> RedirectResponse:
     await BlogService().update_blog(
-        blog_id, title, content, image_file, conn, session_manager
+        author_id, blog_id, title, content, image_file, conn, current_user
     )
     return RedirectResponse(
         url=f"/blogs/show/{blog_id}", status_code=status.HTTP_303_SEE_OTHER
@@ -121,12 +118,11 @@ async def update_blog(
 
 @router.delete("/delete/{blog_id}")
 async def delete_blog(
-    request: Request,
     blog_id: int,
     conn: AsyncConnection = Depends(get_connection_db),
-    session_manager: SessionManager = Depends(SessionManager),
+    current_user: UserData = Depends(get_current_user),
 ) -> JSONResponse:
-    await BlogService().delete_blog(blog_id, conn, session_manager)
+    await BlogService().delete_blog(blog_id, conn, current_user)
     return JSONResponse(
         content={"message": "Blog deleted successfully"}, status_code=status.HTTP_200_OK
     )
