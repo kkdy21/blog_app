1d9e37c5cee185263567b5b84d02ce11fb753620
kkdy21 <kkdy21@naver.com>
2025-08-29 00:45:07 +0900

    feat: orm export

---
+++ src/model/__init__.py
@@ -0,0 +1,8 @@
+# 이 파일은 Alembic이 모든 모델을 인식할 수 있도록
+# 각 ORM 모델을 임포트하는 역할을 합니다.
+# 새로운 ORM 모델을 추가할 때마다 여기에 임포트 구문을 추가해야 합니다.
+
+from .base import Base
+from .blog.orm import Blog
+from .comment.orm import Comment
+from .user.orm import User
